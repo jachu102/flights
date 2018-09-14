8961717d7ab9d5a57138851a222897e38763ecee
Komputer <Komputer@Komputer-PC>
2018-09-14 10:46:04 +0200

    Add initial values.

--- flights/src/main/java/com/bsd/exampleapp/springboot/flights/controler/FlightsController.java
+++ flights/src/main/java/com/bsd/exampleapp/springboot/flights/controler/FlightsController.java
@@ -1,6 +1,7 @@
 package com.bsd.exampleapp.springboot.flights.controler;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FlightsController {
 	
-	List<String> flights = new ArrayList<String>();
+	String[] defaultValues = {"flight-1", "flight-2", "flight-3"};
+	List<String> flights = new ArrayList<String>(Arrays.asList(defaultValues));
 	
 	@RequestMapping(path="/add")
 	public String add(@RequestParam(value="name") String name) {
